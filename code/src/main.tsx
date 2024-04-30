35d3c0e6eb7593275898e1d5a513694774ff3e6b
Jaime Rodriguez <79128716+Maij3@users.noreply.github.com>
2024-04-30 02:09:57 +0200

    [feature/bug-fix] Fix (#7)

--- code/src/main.tsx
+++ code/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from './App'
 import { RouterProvider } from 'react-router-dom'; 
 import { router } from './routes';
 import './index.css'
