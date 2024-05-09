ebabdce373b82e9ef1e5243e42cb608d12f2c386
Jaime Rodriguez <79128716+Maij3@users.noreply.github.com>
2024-05-09 03:49:42 +0200

    Release/1.5.0 (#22)
    * [feature/config-theme] Add theme
    
    * [feature/config-theme] Add theme (#18)
    
    * [feature/config-redux] add config
    
    * [feature/config-redux] add config (#19)
    
    * [Release/v1.5.0] Fix
    
    * Rename App.tsx to app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'; 
 import { router } from './routes';
-import App from "./app";
 import { UIProvider } from '@core/context/ui/UIProvider';
+import App from './app';
 import './index.css'
 
+console.log("render")
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UIProvider>
